5f3963e3e7052cd8dbb388554e9332cdd3f36c44
Nandi Moksnes <nandi.moksnes@gmail.com>
2020-09-23 10:21:32 +0200

    updated emissions + testfile

--- tests/test_file_present.py
+++ tests/test_file_present.py
@@ -13,13 +13,15 @@ modeofoperation = [1, 2]
 class ImportTestCase(unittest.TestCase):
     def test_files_load_csvs_count_nr_loaded_csv_should_be_2(self):
         paths = (os.getcwd() + '\data')
+        print(paths)
         files = load_csvs(paths)
         assert len(files) == 19
 
     def test_noncsv_files_gives_error_message_and_exit(self):
-        paths = (os.getcwd() + '\invalid_test_data')
+        inv_paths = (os.getcwd() + '\invalid_test_data')
+        print(inv_paths)
         with self.assertRaises(SystemExit):
-            load_csvs(paths)
+            load_csvs(inv_paths)
 
     def test_import_OSeMOSYS_param_files_to_outPutFile_hej_hej(self):
         path = (os.getcwd())
@@ -29,6 +31,7 @@ class ImportTestCase(unittest.TestCase):
 
     def test_TRLV_3_operational_life_should_be_60(self):
         paths = (os.getcwd() + '\data')
+        print(paths)
         dict_df = load_csvs(paths)
         param_file = '\osemosys_shell_param.txt'
         path = os.getcwd()
@@ -39,6 +42,7 @@ class ImportTestCase(unittest.TestCase):
 
     def test_MG_5_fixed_cost_should_be_1_in_2027(self):
         paths = (os.getcwd() + '\data')
+        print(paths)
         dict_df = load_csvs(paths)
         param_file = '\osemosys_shell_param.txt'
         path = os.getcwd()
@@ -49,14 +53,26 @@ class ImportTestCase(unittest.TestCase):
 
     def test_SO_3_emissions_should_be_55_CO2_modeoperation_2_in_2027(self):
         paths = (os.getcwd() + '\data')
+        print(paths)
         dict_df = load_csvs(paths)
         param_file = '\osemosys_shell_param.txt'
         path = os.getcwd()
         outPutFile = make_outputfile(path, param_file)
         outPutFile = emissionactivity(dict_df['GIS_data'], outPutFile, startyear, endyear,region, dict_df['emissions'])
-        print(outPutFile)
         test = "region\tSO_3\tCO2\t2\t2027\t55.000000\n"
         assert test in outPutFile
 
+    def test_total_annual_limit_SO_5_should_be_3000(self):
+        paths = (os.getcwd() + '\data')
+        print(paths)
+        dict_df = load_csvs(paths)
+        param_file = '\osemosys_shell_param.txt'
+        path = os.getcwd()
+        print(path)
+        outPutFile = make_outputfile(path, param_file)
+        outPutFile = totaltechnologyannualactivityupperlimit(dict_df['GIS_data'], outPutFile, startyear, endyear,
+                                                             region, dict_df['total_annual_technology_limit'])
+        test = "region\tSO_5\t2027\t3000.000000\n"
+        assert test in outPutFile
 if __name__ == '__main__':
     unittest.main()
